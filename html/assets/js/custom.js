98c11440c72274646384f49aaeaa7dee23e5b90f
azaki <a.zaki@netservex.com>
2020-07-09 02:33:23 +0200

    charts

--- html/assets/js/custom.js
+++ html/assets/js/custom.js
@@ -1,124 +1,183 @@
-var margin = {top: 20, right: 20, bottom: 50, left: 50},
-    width = 1100 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-
-var formatDate = d3.time.format("%Y-%m-%d"); // convert date string to a Date object,
-var formatDate2  = d3.time.format("%Y - %b")
-var y = d3.scale.linear().range([height,0]); //function scale data
-var x = d3.time.scale().range([0, width]);
-/*var x = d3.time.scale();*/
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
-    
-var xAxis = d3.svg.axis() // add axis at tge bottom orientation
-        .scale(x)
-        .orient('bottom')
-        .ticks(d3.time.years, 5)
-        .tickSize(5)
-        .tickFormat(d3.time.format("%Y")); // <-- format
+// new type of chart
 
 
-  var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-10, 0])
-  .html(function(d) {
-    return "<span style='color:red'> $" + d[1] + " Billion </span> </br> <strong>" + formatDate2(d[0])+ "</strong>";
-  });
+var options = {
+    series: [{
+        data: [44, 55, 41, 64, 22, 43, 23, 58, 85, 19]
+    }, {
+    data: [53, 32, 33, 52, 13, 44, 22, 45, 70, 10]
+    },{
+    data: [44, 55, 41, 64, 22, 43, 21, 54, 85, 17]
+    }, {
+    data: [53, 32, 33, 52, 13, 44, 32, 55, 80, 15]
+    }],
+    chart: {
+    type: 'bar',
+    width: '100%',
+    height:300,
+    borderRadius: 10
+  },
+  dataLabels: {
+    enabled: false,
+    offsetX: -6,
+    style: {
+      fontSize: '12px',
+      colors: ['#fff']
+    }
+  },
+  stroke: {
+    show: true,
+    width: 1,
+    colors: ['#fff']
+  },
+  axisBorder: {
+    show: false,
+    color: '#78909C',
+    height: 1,
+    width: '100%',
+    offsetX: 0,
+    offsetY: 0
+},
 
-  //create a chart
-  var chart = d3.select('.chart')
-          .attr('width',width + margin.left + margin.right)
-          .attr('height',height + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); //SVG container
+legend: {show: false},
+yaxis: {show: false},
+grid: {show: false},
+  xaxis: {
+    categories: ['31 May', '30 Apr', '31 Mar', '29 Feb', '20 Dec', '1 jan', '29 sep', '29 July', '29 Nov', '29 Jun'],
+  },
+
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+
+
+  //chart2
+
+
   
-  chart.call(tip);
+var options = {
+    series: [{
+        data: [44, 55, 41, 64, 22, 43, 23, 58, 85, 19]
+    }, {
+    data: [53, 32, 33, 52, 13, 44, 22, 45, 70, 10]
+    },{
+    data: [44, 55, 41, 64, 22, 43, 21, 54, 85, 17]
+    }, {
+    data: [53, 32, 33, 52, 13, 44, 32, 55, 80, 15]
+    }],
+    chart: {
+    type: 'bar',
+    width: '100%',
+    height:300,
+    borderRadius: 10
+  },
+  dataLabels: {
+    enabled: false,
+    offsetX: -6,
+    style: {
+      fontSize: '12px',
+      colors: ['#fff']
+    }
+  },
+  stroke: {
+    show: true,
+    width: 1,
+    colors: ['#fff']
+  },
+  axisBorder: {
+    show: false,
+    color: '#78909C',
+    height: 1,
+    width: '100%',
+    offsetX: 0,
+    offsetY: 0
+},
+
+legend: {show: false},
+yaxis: {show: false},
+grid: {show: false},
+  xaxis: {
+    categories: ['31 May', '30 Apr', '31 Mar', '29 Feb', '20 Dec', '1 jan', '29 sep', '29 July', '29 Nov', '29 Jun'],
+  },
+
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart2"), options);
+  chart.render();
+
+
+
+
+// Horizontal
+
+  var options = {
+    series: [{
+    data: ['12.2%', '93.75%', '34.53%', '31.79%', '23.87%']
+  }],
+    chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      barHeight: 25,
+    }
+  },
   
-  // get JSON data, and build bars based on values
-  d3.json(url, function (json) {
-      
-       //get the max value in data for scale
-   y.domain([0, d3.max(json.data, function(d){
-                return d[1];})]);
-    // format the data
-         json.data.forEach(function(d) {
-        d[0] = formatDate.parse(d[0]);
-        d[1] = +d[1];
-    });
-    
-  x.domain(d3.extent(json.data, function (d) {  return d[0]; }));
-  /*x.range([width/json.data.length/2, width-width/json.data.length/2]);*/
-  
-  chart.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-  
-  //  chart.append("g")
-  //       .attr("class", "y axis")
-  //       .call(yAxis)
-   chart.selectAll(".bar")
-        .data(json.data)
-      .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d[0]); })
-        .attr("width", width/json.data.length)
-        .attr("y", function(d) { return y(d[1]); })
-        .attr("height", function(d) { return height - y(d[1]); })
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
-  });
-  
+  dataLabels: {
+    enabled: false
+  },
+    legend: {show: false},
+    // yaxis: {show: false},
+    grid: {show: false},
+  xaxis: {
+    categories: ['New confirmed cases', 'New Deaths', 'Total cured cases', 'Total confirmed cases', 'Total Deaths'],
+    labels: {
+        show: false,
+    }
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#HorizontalChart"), options);
+  chart.render();
+
+// Horizontal2
+
+var options = {
+    series: [{
+    data: ['12.2%', '93.75%', '34.53%', '31.79%', '23.87%']
+  }],
+    chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      barHeight: 25,
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {show: false},
+  // yaxis: {show: false},
+  grid: {show: false},
+
+  xaxis: {
+    categories: ['New confirmed cases', 'New Deaths', 'Total cured cases', 'Total confirmed cases', 'Total Deaths'],
+    labels: {
+        show: false,
+    }
+  },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#HorizontalChart2"), options);
+  chart.render();
 
 
-//create a chart
-var chart2 = d3.select('.chart2')
-        .attr('width',width + margin.left + margin.right)
-        .attr('height',height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); //SVG container
-
-chart2.call(tip);
-
-// get JSON data, and build bars based on values
-d3.json(url, function (json) {
-    
-     //get the max value in data for scale
- y.domain([0, d3.max(json.data, function(d){
-              return d[1];})]);
-  // format the data
-       json.data.forEach(function(d) {
-      d[0] = formatDate.parse(d[0]);
-      d[1] = +d[1];
-  });
-  
-x.domain(d3.extent(json.data, function (d) {  return d[0]; }));
-/*x.range([width/json.data.length/2, width-width/json.data.length/2]);*/
-
-chart2.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-
-//  chart2.append("g")
-//       .attr("class", "y axis")
-//       .call(yAxis)
- chart2.selectAll(".bar")
-      .data(json.data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d[0]); })
-      .attr("width", width/json.data.length)
-      .attr("y", function(d) { return y(d[1]); })
-      .attr("height", function(d) { return height - y(d[1]); })
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
-});
 
 //init AOS
 
